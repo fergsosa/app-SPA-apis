3e489a4229a2c663f8cd2912768255842c985329
fergsosa <134444526+fergsosa@users.noreply.github.com>
2025-07-02 12:21:37 -0300

    Fix: sintaxis de imports Menu.js

--- app/routers/router.r.js
+++ app/routers/router.r.js
@@ -1,8 +1,8 @@
 import { blogPost, blogPostCargar } from "../controllers/wordpress.c.js";
-import { generadorQR } from "../controllers/generador-qr.c.js";
-import { plantilla } from "../controllers/_plantilla.c.js";
 import { peliShow } from "../controllers/peli-show.c.js";
 import { selectsAnidados } from "../controllers/selects-anidados.c.js";
+import { generadorQR } from "../controllers/generador-qr.c.js";
+// import { plantilla } from "../controllers/_plantilla.c.js";
 // import { faltaCompletar } from "../controllers/faltaCompletar.c.js";
 
 export async function Router() {
@@ -16,7 +16,7 @@ export async function Router() {
   else if (hash.includes("/peliculas")) peliShow($main);
   else if (hash.includes("/selects-anidados")) selectsAnidados($main);
   else if (hash.includes("/generador-qr")) generadorQR($main);
-  else if (hash.includes("/logica,")) plantilla($main);
+  // else if (hash.includes("/logica,")) plantilla($main);
   else blogPostCargar($main);
   //! 🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻🔻
   // else if (hash.includes("/clima")) faltaCompletar($main);
