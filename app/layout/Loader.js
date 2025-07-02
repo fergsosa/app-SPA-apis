9b712d94c99b116f07eebe1da9badb8b803a6d79
fergsosa <134444526+fergsosa@users.noreply.github.com>
2025-07-02 13:06:55 -0300

    Feat: se corrigió el loader

--- app/layout/Loader.js
+++ app/layout/Loader.js
@@ -2,7 +2,7 @@
 export function Loader() {
   const $loader = document.createElement("img");
 
-  $loader.src = "app/assets/img/tail-spin.svg";
+  $loader.src = "./app/assets/img/tail-spin.svg";
   $loader.alt = "Cargando...";
   $loader.classList.add("loader");
   return $loader;
